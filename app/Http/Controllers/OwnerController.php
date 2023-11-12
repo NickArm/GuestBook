1b87e9d2f06cbe63bc0de74feead7a09d8590194
armenisnick <armensnick@gmail.com>
2023-11-12 13:43:23 +0200

    More dev

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 
 class OwnerController extends Controller
